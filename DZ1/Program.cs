5ed0dbb7c20b2f20c1d0ba6ccd7993ab0ecb4981
MariaBezgodkova <ask.2004@yandex.ru>
2023-02-10 12:24:12 +0300

    Первая задача из дз

---
+++ DZ1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее,
+// а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int a = 2;
+int b = 10;
+int max;
+int min;
+if (b > a) 
+{
+    max = b;
+    min = a;
+ Console.Write ("max = ");
+ Console.WriteLine (max);
+ Console.Write ("min = ");
+ Console.WriteLine (min);
+ }
+else 
+{
+    max = a;
+    min = b;
+ Console.Write ("max = ");
+ Console.WriteLine (max);
+ Console.Write ("min = ");
+ Console.WriteLine (min);
+}
